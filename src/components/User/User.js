3f81d91b23812ef74cd8e837381412492403c06f
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-07-08 17:00:40 +0300

    UserDetails were taken in User.js.

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Card, CardHeader, CardImage, CardBody, CardTitle, CartText } from "./styles"
 
 const User = (props) => {
+    
+    const [usersDetails, setUsersDetails] = useState([])
+    useEffect(() => {
+            axios.get(`https://dummyapi.io/data/api/user/${props.user.id}`, { headers: { 'app-id': "60e6ff797b73cc658fe0a67e"}})
+            .then((response) => {
+                setUsersDetails(response.data)
+            })
+    }, [])
+
+    const currentYear = new Date().getFullYear()
+    const dateOfBirth = usersDetails.dateOfBirth && usersDetails.dateOfBirth.slice(0, 4)
+
     return (
         <React.Fragment>
             <Card>
@@ -9,9 +23,9 @@ const User = (props) => {
                     <CardImage src={props.user.picture} alt="randomuser"/>
                 </CardHeader>
                 <CardBody>
-                    <CardTitle>{props.user.firstName} {props.user.lastName}, {props.user.dateOfBirth}</CardTitle>
+                    <CardTitle>{props.user.firstName} {props.user.lastName}, {currentYear - dateOfBirth}</CardTitle>
                     <CartText>{props.user.email}</CartText>
-                    <CartText>Turkey</CartText>
+                    <CartText>{usersDetails.location?.country}</CartText>
                 </CardBody>
             </Card>
         </React.Fragment>
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,24 +5,15 @@ export const UserContext = React.createContext()
 
 const UserContextProvider = (props) => {
     const [users, setUsers] = useState([])
-    // const usersDetails = []
-
+    
     useEffect(() => {
-        axios.get("https://dummyapi.io/data/api/user", { headers: { 'app-id': "60e384660ed5b3b527f81f37"}})
+        axios.get("https://dummyapi.io/data/api/user?limit=10", { headers: { 'app-id': "60e6ff797b73cc658fe0a67e"}})
             .then((response) => {
                 setUsers(response.data.data)
             })
     }, [])
 
-    // useEffect(() => {
-    //     users?.forEach(user => {
-    //         axios.get(`https://dummyapi.io/data/api/user/${user.id}`, { headers: { 'app-id': "60df6baccbce3d7329302078"}})
-    //         .then((response) => {
-    //             usersDetails.push(response.data)
-    //         })
-    //     })
-    // })
-
+    
     return (
         <UserContext.Provider value={{users}}>
             {props.children}
