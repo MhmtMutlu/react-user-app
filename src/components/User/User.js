a445d3acb59add0fcce5e18485e359df52d68fa0
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-07-13 20:58:29 +0300

    Click event was added to Card.

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -6,26 +6,30 @@ import { Card, CardHeader, CardImage, CardBody, CardTitle, CartText } from "./st
 const User = (props) => {
     
     const [usersDetails, setUsersDetails] = useState([])
-    useEffect(() => {
-            axios.get(`https://dummyapi.io/data/api/user/${props.user.id}`, { headers: { 'app-id': "60e6ff797b73cc658fe0a67e"}})
-            .then((response) => {
-                setUsersDetails(response.data)
-            })
-    }, [props.user])
+    function takeDetails() { 
+        
+        axios.get(`https://dummyapi.io/data/api/user/${props.user.id}`, { headers: { 'app-id': "60e6ff797b73cc658fe0a67e"}})
+        .then((response) => {
+            setUsersDetails(response.data)
+        })
+    }
+    
 
     const currentYear = new Date().getFullYear()
     const dateOfBirth = usersDetails.dateOfBirth && usersDetails.dateOfBirth.slice(0, 4)
 
     return (
         <React.Fragment>
-            <Card>
+            <Card onClick={takeDetails}>
                 <CardHeader>
                     <CardImage src={props.user.picture} alt="randomuser"/>
                 </CardHeader>
                 <CardBody>
-                    <CardTitle>{props.user.firstName} {props.user.lastName}, {currentYear - dateOfBirth}</CardTitle>
+                    <CardTitle>{props.user.firstName} {props.user.lastName} {usersDetails.dateOfBirth && `, ${currentYear - dateOfBirth}`}</CardTitle>
                     <CartText>{props.user.email}</CartText>
-                    <CartText>{usersDetails.location?.country}</CartText>
+                    {
+                        usersDetails.location && <CartText>{usersDetails.location?.country}</CartText>
+                    }
                 </CardBody>
             </Card>
         </React.Fragment>
